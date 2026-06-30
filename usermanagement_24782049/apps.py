f9d11a47f994d4d2a08aff32317113ffef8757ab
farelradit <farelradit@gmail.com>
2026-06-30 11:08:46 +0700

    Chore: rapihkan struktur repository

--- usermanagement_24782049/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Usermanagement24782049Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'usermanagement_24782049'
--- usermanagement_24782049/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class User(AbstractUser):
-    is_admin = models.BooleanField(default=False)  # Sesuai poin 1 modul
-    is_member = models.BooleanField(default=True)  # Sesuai poin 1 modul
-
-    def __str__(self):
-        return self.username
